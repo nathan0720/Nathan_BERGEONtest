88d3b22c67c6c19169378b89f36584e16be6765f
nathan0720 <148776376+nathan0720@users.noreply.github.com>
2025-12-13 08:53:25 +0100

    1

---
+++ Projets/Turtel/oeuf.py
@@ -0,0 +1,23 @@
+﻿from turtle import *
+
+r = int(numinput("Choisis", "Taille de l'oeuf (moyen 100)?"))
+
+#pour bien se place au centre
+penup()
+goto(-r, -r/2)
+pendown()
+
+
+right(90)
+write('A')
+circle(r, 180)
+write('B')
+circle(r*2,45)
+write('E')
+# on utilise le théorème de Pythagore
+circle(r*2-(r**2+r**2)**0.5,90)
+write('D')
+circle(r*2,45)
+write('A')
+
+done()
